6c1c3d4afdd05eda368c5f8dfbee178c60c8c365
Malik <67884096+Malik-4444@users.noreply.github.com>
2021-05-04 14:55:28 +0200

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,26 @@
+const db = require("quick.db")
+const config = require('../config.json')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+
+module.exports.run = (client) => {
+    console.log(`Developer: ${config.developer}
+Bot:       ${client.user.username}
+Commands:  ${config.commands}
+Version:   ${config.version}
+Guilds:    ${client.guilds.cache.size}
+Users:     ${client.users.cache.size}
+Libary:    Discord.js
+                   `)
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    
+}).then(console.log('Mongo DB Connected!'))
+
+
+client.user.setActivity(db.get(`status`)); 
+};
